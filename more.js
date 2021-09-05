1ec402c8222d67981d1a476246da7063cedbc43e
linh0927 <88595265+linh0927@users.noreply.github.com>
2021-09-05 23:57:38 +0700

    Create more.js

---
+++ more.js
@@ -0,0 +1,21 @@
+$(document).ready(
+
+function () {
+
+$(".more").slice(0, 10).fadeIn(500);
+
+if ($(".more:hidden").length > 0) {
+$('#loadmore').show();
+}
+
+$("#loadmore").on('click', function (e) {
+e.preventDefault(); 
+
+$(".more:hidden").slice(0, 10).slideDown(1000);
+
+if ($(".more:hidden").length < 1) {
+$("#loadmore").fadeOut("fast");
+}
+
+});
+});
